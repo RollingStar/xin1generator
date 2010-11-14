d281aaac83b42355364c8652d80114e7e29b7d85
daniel.moscoviter@gmail.com <daniel.moscoviter@gmail.com@3fc6a63b-69a8-1dac-7af1-bd5d4a2f0732>
2010-11-14 21:33:40 +0000

    Add all relevant files.

---
+++ trunk/Xin1Generator/Xin1Generator/TagsGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace Xin1Generator {
+    class TagsGenerator {
+        public XDocument document { get; private set; }
+        private XElement root;
+
+        public TagsGenerator() {
+            document =
+                new XDocument(
+                    new XDocumentType("Tags", null, "matroskatags.dtd", null),
+                    root = new XElement("Tags"));
+        }
+
+        public void CreateTag(int editionUID, string title) {
+            root.Add(
+                new XElement("Tag",
+                    new XElement("Targets",
+                        new XElement("EditionUID", editionUID)),
+                    new XElement("Simple",
+                        new XElement("Name", "TITLE"),
+                        new XElement("String", title))));
+        }
+    }
+}
